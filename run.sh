5cd3b1d339d58f5ab3f2cf09045d1804ee707330
Isabela <isabela.zeberio@gmail.com>
2025-04-10 20:45:30 +0200

    Update run.sh and requirements.txt

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Create and activate virtual environment
-python3 -m venv venv
-source venv/bin/activate
+python3 -m venv imdb_venv
+source imdb_venv/bin/activate #macOs/Linux
+# imdb_venv\Scripts\activate #Windows 
+
 
 # Upgrade pip
 pip install --upgrade pip
@@ -11,3 +13,8 @@ pip install -r requirements.txt
 
 # Run the analysis
 python code/imdb.ipynb
+
+# Run the notebook
+jupyter nbconvert --execute --to notebook code/imdb.ipynb
+
+echo "Jupyter notebook executed succesfully"
